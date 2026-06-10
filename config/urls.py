9261294cad88c3d10cdbe3c8952320dc78988b45
Meysam Kermani <meysamkermani@gmail.com>
2026-06-10 21:02:10 +0330

    update urls structure

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('apps.blog.urls')),
+    path('shop/', include('apps.shop.urls')),
+    path('common/', include('apps.common.urls'))
+
 ]
