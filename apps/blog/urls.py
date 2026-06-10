2a10b79d9b7985002d162ecc67200bd0506325f3
Meysam Kermani <meysamkermani@gmail.com>
2026-06-10 21:19:24 +0330

    add blog urls

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+
 app_name = "apps.blog"
 
 urlpatterns = [
+    path("", "post_list"),
+    path("post/<slug:slug>", "post_detail"),
+
 ]
