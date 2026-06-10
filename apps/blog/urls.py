482b6e9ff188345c6dd6efa81efff3716043fe09
Meysam Kermani <meysamkermani@gmail.com>
2026-06-10 21:27:11 +0330

    add post views

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-
+from apps.blog.views import PostDetailView, PostListView
 app_name = "apps.blog"
 
 urlpatterns = [
-    path("", "post_list"),
-    path("post/<slug:slug>", "post_detail"),
+    path("", PostListView.as_view() ,"post_list"),
+    path("post/<slug:slug>", PostDetailView.as_view() , "post_detail"),
 
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from apps.blog.models import Post
 
-# Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = ""
+    context_object_name = "posts"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = ""
+    context_object_name = "post"
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'    
+
