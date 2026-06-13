8bfc4ff65265fb46d68cb8206fb010edb05d1f97
Meysam Kermani <meysamkermani@gmail.com>
2026-06-13 20:33:04 +0330

    modify shop urls

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from apps.shop.views import ProductDetailView, ProductListView
 app_name = "apps.shop"
 
 urlpatterns = [
+    path("", ProductListView.as_view() ,name="product_list"),
+    path("product/<slug:slug>", ProductDetailView.as_view() , name="product_detail"),
+
 ]
