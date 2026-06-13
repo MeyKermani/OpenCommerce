c2e2fb82fb5ebdcc1063d22a80441a2c7f6c426c
Meysam Kermani <meysamkermani@gmail.com>
2026-06-13 20:28:46 +0330

    implement Product CRUD

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from apps.shop.models import Product
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "shop/product/list.html"
+    context_object_name = "products"
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "shop/product/detail.html"
+    context_object_name = "product"
+    slug_field = 'slug'
+    slug_url_kwargs = 'slug'
