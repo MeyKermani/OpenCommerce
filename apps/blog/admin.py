5f272a55d1485fc01a20ed86c1883d5bc672616d
Meysam Kermani <meysamkermani@gmail.com>
2026-06-13 21:06:22 +0330

    register blog entities into django admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from apps.blog.models import PostCategory, Post
 
-admin.site.register(PostCategory)
-admin.site.register(Post)
+
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title','created_at', 'updated_at')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('title',)
+    
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category','created_at', 'updated_at')
+    search_fields = ('title','slug')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('title',)
