78b1859eff7464c682615fc0ec2e49e99c9e1d5d
Meysam Kermani <meysamkermani@gmail.com>
2026-06-17 21:08:53 +0330

    register Tag model in common/admin

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.common.models import Tag
 
-# Register your models here.
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title','slug', 'updated_at')
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ('title',)
+    
